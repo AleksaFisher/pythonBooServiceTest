a4f0afbe89f91b89ef52f2dad9796858e1b8ce1f
afisher <afisher@griddynamics.com>
2022-03-31 16:58:00 +0300

    init

--- test_change_name_book.py
+++ test_change_name_book.py
@@ -1,10 +1,10 @@
 import requests
 
-def test_change_name_book():
-    dt_change_book = {"type": "Drama"}
+def test_change_name_book(bookid):
+    dt_change_book = {"id":bookid,"type": "Drama"}
     getid = None
     try:
-        response = requests.put("http://127.0.0.1:5000/v1/books/manipulation", json=dt_change_book)
+        response = requests.put("http://127.0.0.1:5000/v1/books/manipulation?id={}".format(bookid), json=dt_change_book)
         print("{}\n{}".format(response.status_code, response.json()))
         getid = response.json()['id']
         print("{}".format(getid))
@@ -14,4 +14,4 @@ def test_change_name_book():
 
 if __name__ == '__main__':
         print('PyCharm')
-        test_change_name_book()
+        test_change_name_book("bf7da2c6-40e3-47ec-bb20-c81077f123ef")
