c7d1fac7325a68ddcb4c00ff2d266939b9856b8c
afisher <afisher@griddynamics.com>
2022-04-03 14:58:08 +0300

    create module

--- test_books_service_api/__init__.py
+++ test_books_service_api/__init__.py
@@ -1,6 +1,6 @@
-# import delete_book
+from .delete_book import *
 from .test_add_book import *
-# import test_get_info
-# import test_get_limit
+from .test_get_info import *
+from .test_get_limit import *
 from .test_get import *
-# import test_change_name_book
+from .test_change_name_book import *
