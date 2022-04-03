2520c9f118ebd9d1f88d5d42ccc86afb6c132db2
afisher <afisher@griddynamics.com>
2022-04-03 16:30:31 +0300

    create module

--- test_books_service_api_class/test_api_get.py
+++ test_books_service_api_class/test_api_get.py
@@ -1,14 +1,31 @@
 import requests
 
+
 class TestApiGet:
     def __init__(self, url, data):
         self.url = url
         self.data = data
 
-    def GetBookInfo(self):
+    def GetLatestBookInfo(self):
         response = None
         try:
             response = requests.get("{}/latest?limit={}".format(self.url, self.data)).json()
+        except Exception:
+            print("{}\n{}".format(response.status_code, response.json()))
+        return response
+
+    def GetBookInfoByType(self):
+        response = None
+        try:
+            response = requests.get("{}/ids?book_type={}".format(self.url, self.data)).json()
+        except Exception:
+            print("{}\n{}".format(response.status_code, response.json()))
+        return response
+
+    def GetBookInfoById(self):
+        response = None
+        try:
+            response = requests.get("{}/info?id={}".format(self.url, self.data)).json()
         except Exception:
             print("{}\n{}".format(response.status_code, response.json()))
         return response
