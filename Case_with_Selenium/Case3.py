aa1f34e50216856d4bb86a3421339f30fa28976b
afisher <afisher@griddynamics.com>
2022-04-11 14:09:02 +0300

    selenium case 1

--- Case_with_Selenium/Case3.py
+++ Case_with_Selenium/Case3.py
@@ -12,7 +12,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 class TestCase4():
   def setup_method(self, method):
-    self.driver = webdriver.Chrome()
+    self.driver = webdriver.Chrome("/Users/afisher/Downloads/chrome_drivers/98/chromedriver")
     self.vars = {}
   
   def teardown_method(self, method):
